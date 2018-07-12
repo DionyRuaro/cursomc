dcacd01986af8c02a7b17bf91c938b4fd0e8f404
Diony Ruaro <diony.ruaro@rduphold.com.br>
2018-07-12 10:04:36 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ruaro/cursomc/CursomcApplication.java
+++ src/main/java/com/ruaro/cursomc/CursomcApplication.java
@@ -66,11 +66,11 @@ public class CursomcApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Rio de Janeiro");
 		
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
+		Cidade c2 = new Cidade(null, "Rio de Janeiro", est2);
+		Cidade c3 = new Cidade(null, "Niterói", est2);
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
